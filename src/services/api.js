65cf918ef8d77a62b8de74fbc0d44ff4f646afe5
Sergey Laletin <Quercue@gmail.com>
2025-06-20 17:57:09 +0300

    feat: create auth.js

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL = "https://wedev-api.sky.pro/api/kanban";
 
-export async function fetchWords({ token }) {
+export async function fetchCards({ token }) {
   try {
     const data = await axios.get(API_URL, {
       headers: {
